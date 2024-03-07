9dd73342d3e7c24fb354243e1ed95d6d38abf8e8
BassamEmad39 <besoemad16@gmail.com>
2024-03-07 22:51:31 +0200

    commited first time

---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView
+({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor:Colors.yellow,),
+      body: Column(children: [Center(child: Text("Home Page")),
+      Center(child: Text("Home Page")),
+      Center(child: Text("Home Page")),
+      Center(child: Text("Home Page")),
+      ],),
+    );
+  }
+}
