3f8d06619bc09cfa55d93605eb519136f41b966f
BassamEmad39 <besoemad16@gmail.com>
2024-03-10 00:06:27 +0200

    updates by bassam

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: MainAppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  Center(
+                    child: CircleAvatar(
+                      radius: 100.r,
+                      backgroundImage: AssetImage('images/suits.jpg'),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                      'يعد نشر البحث العلمي من المهام الأكثر الأهمية التي من المفترض أن يضطلع بها أعضاء هيئة التدريس والباحثون لدى جامعة كفر الشيخ .'),
+                  Text(
+                      'لذا وُضِع برنامج الجوائز البحثية بغية الإشادة بالجهود المبذولة من جانب الباحثين المتميزين في جامعة كفر الشيخ وتحفيزهم على بذل المزيد من أجل الارتقاء بمجال البحوث الأكاديمية وإجراء المزيد من البحوث ورفع المستوى الأكاديمي للجامعة بحيث تكون في مصاف الجامعات العالمية.'),
+                  SizedBox(
+                    height: 6.h,
+                  ),
+                  Center(child: Image.asset('images/200pound.gif')),
+                  SizedBox(
+                    height: 6.h,
+                  ),
+                  Text(
+                    'تعليمات هامة:',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Color(0xff1A5653)),
+                  ),
+                  Text(
+                    'علي السادة المتقدمين لجوائز النشر الدولي تقديم:',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 5.h,
+                  ),
+                  Text(
+                      'صورة من صفحة جوجل سكولار (Google Scholar) من الموقع الإلكتروني scholar.google.com الخاص بالمتقدم'),
+                  Text(
+                      'ان يكون الانتماء الأكاديمي (Affiliation) يحتوي علي أسم جامعة كفر الشيخ  كالتالي “Kafrelsheikh University” '),
+                  Text(
+                      'ان يكون البريد الالكتروني (Verified Email) يحتوي على نطاق جامعة كفر الشيخ على الانترنت وهو “kfs.edu.eg”'),
+                  SizedBox(
+                    height: 40.h,
+                  ),
+                  Center(
+                    child: Column(
+                      children: [
+                        Text('جوائز النشر العلمي في ارقام'),
+                        Text('إحصائيات تظهر حجم الجوائز في جامعة كفر الشيخ'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 5,),
+                  Row(
+                    children: [
+                      Column(
+                        children: [
+                          Align(
+                            alignment: AlignmentDirectional.topCenter,
+                            child: CircleAvatar(
+                              radius: 10,
+                              backgroundColor: Color(0xff1A5653),
+                              foregroundColor: Colors.white,
+                              child: Container(
+                                child: Image.asset('images/group.png',color: Colors.white,height: 20,width: 20,),
+                              ),
+                            ),
+                          ),
+
+                          Container(
+                            padding: EdgeInsets.only(top: 6),
+                            height: 55.h,
+                            width: 90.w,
+                            color: Color(0xff999999),
+                            child: Column(
+                              children: [Text('100'),
+                                Text('دكتور',style: TextStyle(fontSize: 10),),],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Align(
+                              alignment: AlignmentDirectional.topCenter,
+                              child: CircleAvatar(
+                                radius: 10,
+                                backgroundColor: Color(0xff1A5653),
+                                foregroundColor: Colors.white,
+                                child: Container(
+                                  child: Image.asset('images/contact.png',color: Colors.white,),
+                                ),
+                              ),
+                            ),
+                        
+                            Container(
+                              padding: EdgeInsets.only(top: 6),
+                              height: 55.h,
+                              width: 90.w,
+                              color: Color(0xff999999),
+                              child: Column(
+                                children: [Text('1700'),
+                                  Text('نشر',style: TextStyle(fontSize: 10),),],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Align(
+                            alignment: AlignmentDirectional.topCenter,
+                            child: CircleAvatar(
+                              radius: 10,
+                              backgroundColor: Color(0xff1A5653),
+                              foregroundColor: Colors.white,
+                              child: Container(
+                                child: Image.asset('images/dollar.png',color: Colors.white,),
+                              ),
+                            ),
+                          ),
+
+                          Container(
+                            padding: EdgeInsets.only(top: 6),
+                            height: 55.h,
+                            width: 90.w,
+                            color: Color(0xff999999),
+                            child: Column(
+                              children: [Text('2000000'),
+                                Text('قيمة الجوائز المدفوعة',style: TextStyle(fontSize: 10),),],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 50,)
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MainAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.only(left: 8, right: 8),
+        height: 70.h,
+        child: Row(
+          children: [
+            InkWell(
+              onTap: () {},
+              child: Ink(
+                height: 45.h,
+                width: 45.h,
+                color: Color(0xff1A5653),
+                child: Center(
+                    child: Text(
+                  'EN',
+                  style: TextStyle(color: Colors.white),
+                )),
+              ),
+            ),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.menu,
+                  color: Color(0xff1A5653),
+                  size: 30.0,
+                )),
+            Expanded(
+              child: Center(
+                child: Text(
+                  'جوائز النشر العلمي',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff1A5653),
+                      fontSize: 18.sp),
+                ),
+              ),
+            ),
+            Container(
+              child: Image.asset(
+                'images/logo (1).png',
+                height: 36.h,
+                width: 90.w,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(60.h);
+}
